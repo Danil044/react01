fcb5b97896cf8b2063903b0fb224d723b0d20877
ACER <ACER@DESKTOP-B187N13>
2022-04-28 15:30:01 +0300

    localStorage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
 import MyReactive from "./components/MyReactive";
-import MyArray from "./components/MyArray";
+//import MyArray from "./components/MyArray";
+
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
+import PageAbout from "./pages/About"
+import PageContact from "./pages/Contact"
+import PageHome from "./pages/Home"
+import PageError404 from "./pages/Error404"
+import PagePortfolios from "./pages/Portfolios"
+
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-        <MyArray></MyArray>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+            <Header></Header>
+                <Routes>
+                    <Route path="/" element={<PageHome />} />
+                    <Route path="about" element={<PageAbout />} />
+                    <Route path="contact" element={<PageContact />} />
+                    <Route path="portfolios" element={<PagePortfolios />} />
+                    <Route path="*" element={<PageError404 />} />
+                </Routes>
+            <Footer></Footer>
+        </div>
+      </BrowserRouter>
   );
 }
 
